1afb02a70bb3fa639eaed42c85c4d5a4615641b2
Michał Worwąg <michalwo@gmail.com>
2019-01-31 18:10:51 +0100

    Add components, delete unnecessary files

---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { recipe } from "../temDetails";
+
+export default class RecipeDetails extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello From Details</h1>
+      </React.Fragment>
+    );
+  }
+}
