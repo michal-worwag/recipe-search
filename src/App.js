b26f3123d414128b3aaf58a79de915f2542555af
Michał Worwąg <michalwo@gmail.com>
2019-02-04 19:05:12 +0100

    Showing basic recipe list

--- src/App.js
+++ src/App.js
@@ -7,10 +7,32 @@ import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 class App extends Component {
+  state ={
+    recipes: recipes,
+    url: "https://www.food2fork.com/api/search?key=33ddbfc44b41e62304b02d0c44f67326"
+  };
+
+  // async getRecpies() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+
+  //     this.setState({ recipes: jsonData.recipes })
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentDidMount(){
+  //   this.getRecpies();
+  // }
+
   render() {
+    // console.log(this.state.recipes);
+
     return (
       <React.Fragment>
-        <RecipeList />
+        <RecipeList recipes={this.state.recipes} />
         <RecipeDetails />
       </React.Fragment>
     );
