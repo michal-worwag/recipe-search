65e6002e58083bfc2ef6bc0daf992f8919bd0a3d
Michał Worwąg <michalwo@gmail.com>
2019-02-08 18:44:07 +0100

    Updates

---
+++ precache-manifest.d6bcae67fb4cc25ee573c998e4f46d45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab85c097c04af170c38d",
+    "url": "/recipe-search/static/css/main.3c35ad64.chunk.css"
+  },
+  {
+    "revision": "ab85c097c04af170c38d",
+    "url": "/recipe-search/static/js/main.ab85c097.chunk.js"
+  },
+  {
+    "revision": "fea9c8b34d6e3b807e18",
+    "url": "/recipe-search/static/js/1.fea9c8b3.chunk.js"
+  },
+  {
+    "revision": "f7e68c8e6ab53071e650",
+    "url": "/recipe-search/static/js/runtime~main.f7e68c8e.js"
+  },
+  {
+    "revision": "99e7225232abc763e51b0042da38f34f",
+    "url": "/recipe-search/index.html"
+  }
+];
