ab09dbf72832bc518847da9328e2ca68082d4b31
Michał Worwąg <michalwo@gmail.com>
2019-02-08 18:40:05 +0100

    App finished

--- src/App.js
+++ src/App.js
@@ -11,16 +11,28 @@ class App extends Component {
     recipes: recipes,
     url:
       "https://www.food2fork.com/api/search?key=33ddbfc44b41e62304b02d0c44f67326",
+    base_url:
+      "https://www.food2fork.com/api/search?key=33ddbfc44b41e62304b02d0c44f67326",
     details_id: 35384,
-    pageIndex: 1
+    pageIndex: 1,
+    search: "",
+    query: "&q=",
+    error: ""
   };
 
   async getRecpies() {
     try {
       const data = await fetch(this.state.url);
       const jsonData = await data.json();
-
-      this.setState({ recipes: jsonData.recipes });
+      if (jsonData.recipes.length === 0) {
+        this.setState(() => {
+          return { error: "Sorry, recipe doesn't exist" };
+        });
+      } else {
+        this.setState(() => {
+          return { recipes: jsonData.recipes };
+        });
+      }
     } catch (error) {
       console.log(error);
     }
@@ -38,6 +50,10 @@ class App extends Component {
           <RecipeList
             recipes={this.state.recipes}
             handleDetails={this.handleDetails}
+            value={this.state.search}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            error={this.state.error}
           />
         );
       case 0:
@@ -61,6 +77,26 @@ class App extends Component {
       pageIndex: index,
       details_id: id
     });
+  };
+
+  handleChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { base_url, query, search } = this.state;
+
+    this.setState(
+      () => {
+        return { url: `${base_url}${query}${search}`, search: "" };
+      },
+      () => {
+        this.getRecpies();
+      }
+    );
   };
 
   render() {
